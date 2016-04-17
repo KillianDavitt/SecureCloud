9208e850fa8328957f2d90d40f99ebf128d7a21f
Killian Davitt <davittk@tcd.ie>
2016-04-17 21:01:14 +0100

    Added comments to network functions

--- network/network.go
+++ network/network.go
@@ -8,11 +8,12 @@ import (
 	"net"
 )
 
+// Send bytes to a given connection
 func Send(conn net.Conn, data []byte) {
 	size := len(data)
-	size_bytes := make([]byte, 8)
-	binary.LittleEndian.PutUint64(size_bytes, uint64(size))
-	_, err := conn.Write(size_bytes)
+	sizeBytes := make([]byte, 8)
+	binary.LittleEndian.PutUint64(sizeBytes, uint64(size))
+	_, err := conn.Write(sizeBytes)
 	if err != nil {
 		fmt.Println("Error in query")
 		log.Fatal(err)
@@ -24,14 +25,15 @@ func Send(conn net.Conn, data []byte) {
 	}
 }
 
+// Recieve bytes from a give connection
 func Receive(c net.Conn) []byte {
-	size_bytes := make([]byte, 8)
-	_, err := io.ReadFull(c, size_bytes)
+	sizeBytes := make([]byte, 8)
+	_, err := io.ReadFull(c, sizeBytes)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	size := binary.LittleEndian.Uint64(size_bytes)
+	size := binary.LittleEndian.Uint64(sizeBytes)
 	fmt.Printf("\nGot a size: %d", size)
 	data := make([]byte, size)
 	_, err = io.ReadFull(c, data)
